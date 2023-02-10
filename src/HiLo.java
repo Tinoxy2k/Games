140c19ae4c07113936f9bb24e41dbd01af575a59
Tinoxy2k <51656455+Tinoxy2k@users.noreply.github.com>
2023-02-11 00:55:22 +0100

    Add scanner

--- src/HiLo.java
+++ src/HiLo.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class HiLo {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         int theNumber = (int)(Math.random() * 100 + 1);
         System.out.println(theNumber);
+        int guess = 0;
+
+
     }
 }
