41f7b3d0085e475f57fd103d40dc0cc81f6b27a1
Tinoxy2k <51656455+Tinoxy2k@users.noreply.github.com>
2023-02-11 01:08:34 +0100

    Add block while

--- src/HiLo.java
+++ src/HiLo.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
+
 public class HiLo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int theNumber = (int)(Math.random() * 100 + 1);
+        int theNumber = (int) (Math.random() * 100 + 1);
         System.out.println(theNumber);
         int guess = 0;
-
+        while (guess != theNumber) {
+            System.out.println("Guess a number between 1 and 100:");
+            guess = scanner.nextInt();
+            System.out.println("You entered " + guess + ".");
+        }
 
     }
 }
