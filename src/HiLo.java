3432edb3128ba12aee48b6fedb64918ac8f21b31
Tinoxy2k <51656455+Tinoxy2k@users.noreply.github.com>
2023-02-11 01:19:17 +0100

    Add block if else

--- src/HiLo.java
+++ src/HiLo.java
@@ -4,11 +4,16 @@ public class HiLo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int theNumber = (int) (Math.random() * 100 + 1);
-        System.out.println(theNumber);
+      //  System.out.println(theNumber);
         int guess = 0;
         while (guess != theNumber) {
             System.out.println("Guess a number between 1 and 100:");
             guess = scanner.nextInt();
+            if(guess < theNumber)
+                System.out.println(guess + " is too low. Try again");
+            else if (guess > theNumber)
+                System.out.println(guess + " is too high. Try again");
+            else System.out.println(guess + " is correct. You win!");
             System.out.println("You entered " + guess + ".");
         }
 
