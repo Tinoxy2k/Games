d9fff29e8f40cea18d41f58ca0b3a04e2281feab
Tinoxy2k <51656455+Tinoxy2k@users.noreply.github.com>
2023-02-11 01:41:15 +0100

    add block do-while

--- src/HiLo.java
+++ src/HiLo.java
@@ -3,19 +3,24 @@ import java.util.Scanner;
 public class HiLo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int theNumber = (int) (Math.random() * 100 + 1);
-      //  System.out.println(theNumber);
-        int guess = 0;
-        while (guess != theNumber) {
-            System.out.println("Guess a number between 1 and 100:");
-            guess = scanner.nextInt();
-            if(guess < theNumber)
-                System.out.println(guess + " is too low. Try again");
-            else if (guess > theNumber)
-                System.out.println(guess + " is too high. Try again");
-            else System.out.println(guess + " is correct. You win!");
-            System.out.println("You entered " + guess + ".");
-        }
-
+        String playAgain = "";
+        do {
+            int theNumber = (int) (Math.random() * 100 + 1);
+            //  System.out.println(theNumber);
+            int guess = 0;
+            while (guess != theNumber) {
+                System.out.println("Guess a number between 1 and 100:");
+                guess = scanner.nextInt();
+                if (guess < theNumber)
+                    System.out.println(guess + " is too low. Try again");
+                else if (guess > theNumber)
+                    System.out.println(guess + " is too high. Try again");
+                else System.out.println(guess + " is correct. You win!");
+                System.out.println("You entered " + guess + ".");
+            }
+            System.out.println("Would you like to play again? (y/n)?");
+            playAgain = scanner.next();
+        }while (playAgain.equalsIgnoreCase("y"));
+        System.out.println("thank you for playing! Goodbye.");
     }
 }
